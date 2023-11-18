89071d173b9874e85e40a0137bfcbd5b8c297942
ALLYSSON <139386580+Allysson2023@users.noreply.github.com>
2023-11-18 10:09:56 -0300

    Create infos.php

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "concessionaria.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "GbBKqzMI50gC8gENzL4T";
+$database = "loja";
+?>
